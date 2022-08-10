1b87f001d8f546561524822d77615de676ca173d
996Worker <69796042+Blackmesa-Canteen@users.noreply.github.com>
2022-08-11 00:01:27 +1000

    removed useless demo codes

--- src/backend-project/src/main/java/io/swen90007sm2/app/controller/shared/api/MainController.java
+++
@@ -1,4 +0,0 @@
-package io.swen90007sm2.app.controller.shared.api;
-
-public class MainController {
-}
--- src/backend-project/src/main/java/io/swen90007sm2/app/dao/IDemoDao.java
+++
@@ -1,7 +0,0 @@
-package io.swen90007sm2.app.dao;
-
-import io.swen90007sm2.app.dao.impl.DemoDao;
-
-public interface IDemoDao {
-    DemoDao getDemoById(Long id);
-}
