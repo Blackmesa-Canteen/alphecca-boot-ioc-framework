9a8ca7d585f32fc057e05209cd8f5e95aa1dda75
996Worker <69796042+Blackmesa-Canteen@users.noreply.github.com>
2022-08-16 12:41:45 +1000

    changed cache expiration time

--- src/backend-project/src/main/java/io/swen90007sm2/app/cache/constant/CacheConstant.java
+++ src/backend-project/src/main/java/io/swen90007sm2/app/cache/constant/CacheConstant.java
@@ -9,10 +9,11 @@ public interface CacheConstant {
     String OBJECT_CACHE_BEAN = "object-cache-bean";
     String STRING_CACHE_BEAN = "string-cache-bean";
 
-    // normal data expiration time max is 1 Min
+    // normal data expiration time max is 3 Min
     // need to use random period to prevent cache avalanche
-    Long CACHE_NORMAL_EXPIRATION_PERIOD_MAX = (long) (60 * 1000);
-    // Hot data expiration time max is 10 Second
+    Long CACHE_NORMAL_EXPIRATION_PERIOD_MAX = (long) (3 * 60 * 1000);
+    // Hot data expiration time max is 30 Second,
+    // hot data is the data being updated often
     // need to use random period to prevent cache avalanche
-    Long CACHE_HOT_EXPIRATION_PERIOD_MAX = (long) (10 * 1000);
+    Long CACHE_HOT_EXPIRATION_PERIOD_MAX = (long) (30 * 1000);
 }
