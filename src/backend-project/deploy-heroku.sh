e0e030264d1e431502cb416a6f3dd0c57b949532
996Worker <69796042+Blackmesa-Canteen@users.noreply.github.com>
2022-08-18 23:11:02 +1000

    add deployment scripts

---
+++ src/backend-project/deploy-heroku.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# firstly, please login the heroku
+heroku login
+
+# then, login the heroku container
+heroku container:login
+
+# then build the docker thing
+docker build -t swen90007-backend-app .
+
+# tag the image to heroku repo
+docker tag swen90007-backend-app:latest registry.heroku.com/swen90007-backend-app/web
+
+# push the repo to heroku
+docker push registry.heroku.com/swen90007-backend-app/web
+
+# release the container
+heroku container:release web --app swen90007-backend-app
+
+# then it works
