8d3cffdfc4e4253e00a879fc0861734fe84feeaf
Lishun Yang <lishuny@student.unimelb.edu.au>
2022-09-17 17:58:49 +1000

    finalizing search results

--- src/frontend-project/src/components/search/Search.js
+++ src/frontend-project/src/components/search/Search.js
@@ -1,11 +1,12 @@
 import {
+  AmenityContainer,
+  AmenityIcon,
   SearchCancelOp,
   SearchCheckedBtn,
   SearchContainer,
   SearchDescription,
   SearchDetails,
   SearchDetailWrap,
-  SearchDistance,
   SearchFeatures,
   SearchImg,
   SearchOpSubtitle,
@@ -14,42 +15,82 @@ import {
   SearchRatingBtn,
   SearchRatingSpan,
   SearchSubtitle,
-  SearchTaxiOp,
   SearchTaxOp,
   SearchTitle,
 } from "./SearchElements";
+import {
+  faDumbbell,
+  faMartiniGlass,
+  faMugSaucer,
+  faWifi,
+  faFan,
+  faShirt,
+  faCloudSun,
+  faSquareParking,
+  faPersonSwimming,
+  faUtensils,
+  faBellConcierge,
+} from "@fortawesome/free-solid-svg-icons";
 
 const Search = ({ item }) => {
+  const ShowIcon = (description) => {
+    if (description === "Air Condition") {
+      return <AmenityIcon icon={faFan} alt="Air Conditioner" />;
+    }
+    if (description === "Bar") {
+      return <AmenityIcon icon={faMartiniGlass} alt="Bar" />;
+    }
+    if (description === "Breakfast") {
+      return <AmenityIcon icon={faMugSaucer} alt="Breakfast" />;
+    }
+    if (description === "Free Wifi") {
+      return <AmenityIcon icon={faWifi} alt="Free Wifi" />;
+    }
+    if (description === "Gym") {
+      return <AmenityIcon icon={faDumbbell} alt="Gym" />;
+    }
+    if (description === "Laundry") {
+      return <AmenityIcon icon={faShirt} alt="Laundry" />;
+    }
+    if (description === "Outdoor") {
+      return <AmenityIcon icon={faCloudSun} alt="Outdoor" />;
+    }
+    if (description === "Parking") {
+      return <AmenityIcon icon={faSquareParking} alt="Parking" />;
+    }
+    if (description === "Swimming Pool") {
+      return <AmenityIcon icon={faPersonSwimming} alt="Swimming Pool" />;
+    }
+    if (description === "Restaurant") {
+      return <AmenityIcon icon={faUtensils} alt="Restaurant" />;
+    }
+    if (description === "Room Service") {
+      return <AmenityIcon icon={faBellConcierge} alt="Room Service" />;
+    }
+  };
+
   return (
     <SearchContainer>
       <SearchImg
-        src="https://cf.bstatic.com/xdata/images/hotel/square600/261707778.webp?k=fa6b6128468ec15e81f7d076b6f2473fa3a80c255582f155cae35f9edbffdd78&o=&s=1"
+        src="https://dummyimage.com/420x320/ff7f7f/333333.png&text=Sample"
         alt=""
       />
       <SearchDescription>
-        <SearchTitle className="siTitle">Tower Street Apartments</SearchTitle>
-        <SearchDistance className="siDistance">500m from center</SearchDistance>
-        <SearchTaxiOp>Free airport taxi</SearchTaxiOp>
-        <SearchSubtitle>Studio Apartment with Air conditioning</SearchSubtitle>
-        <SearchFeatures className="siFeatures">
-          Entire studio • 1 bathroom • 21m² 1 full bed
-        </SearchFeatures>
-        <SearchCancelOp>Free cancellation </SearchCancelOp>
+        <SearchTitle>{item.name}</SearchTitle>
+        <SearchSubtitle>Amenities:</SearchSubtitle>
+        <AmenityContainer>
+          {item.amenities.map((amenity) => ShowIcon(amenity.description))}
+        </AmenityContainer>
+        <SearchCancelOp>Great Price! </SearchCancelOp>
         <SearchOpSubtitle>
-          You can cancel later, so lock in this great price today!
+          Get the most valuable price for today!
         </SearchOpSubtitle>
       </SearchDescription>
       <SearchDetails>
-        <SearchRating>
-          <SearchRatingSpan>Excellent</SearchRatingSpan>
-          <SearchRatingBtn>8.9</SearchRatingBtn>
-        </SearchRating>
         <SearchDetailWrap>
-          <SearchPrice>$124</SearchPrice>
+          <SearchPrice>$299</SearchPrice>
           <SearchTaxOp>Includes taxes and fees</SearchTaxOp>
-          <SearchCheckedBtn className="siCheckButton">
-            See availability
-          </SearchCheckedBtn>
+          <SearchCheckedBtn>Check Details</SearchCheckedBtn>
         </SearchDetailWrap>
       </SearchDetails>
     </SearchContainer>
