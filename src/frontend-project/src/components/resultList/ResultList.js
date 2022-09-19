0ef6959c14b2976f9c136fcad2871d0addd4499f
Lishun Yang <lishuny@student.unimelb.edu.au>
2022-09-19 12:41:31 +1000

    fixing data fetching function bugs

--- src/frontend-project/src/components/resultList/ResultList.js
+++ src/frontend-project/src/components/resultList/ResultList.js
@@ -3,31 +3,27 @@ import Tab from "react-bootstrap/Tab";
 import Tabs from "react-bootstrap/Tabs";
 import { GetBookings } from "../../API/CustomerApi";
 import Booking from "../../components/bookings/Booking";
+import { UseAllTransaction } from "../../API/CustomerApi";
 
 const ResultList = (customer) => {
-  const { booking, loading, error } = GetBookings(customer.item);
+  const { loading, transaction, error } = UseAllTransaction(customer.item);
 
-  if (booking.msg === "User Not Exist.") {
-    return <div>loading</div>;
-  } else {
-    return (
-      <div>
-        {console.log(booking)}
-        <Tabs defaultActiveKey="profile" id="uncontrolled-tab-example">
-          <Tab eventKey="profile" title="Profile">
-            My profile
-          </Tab>
-          <Tab eventKey="booking" title="My Booking">
-            {/* <div>
+  return (
+    <div>
+      <Tabs defaultActiveKey="profile" id="uncontrolled-tab-example">
+        <Tab eventKey="profile" title="Profile">
+          My profile
+        </Tab>
+        <Tab eventKey="booking" title="My Booking">
+          {/* <div>
               {booking.data.map((item) => (
                 <Booking item={item} key={item.id} />
               ))}
             </div> */}
-          </Tab>
-        </Tabs>
-      </div>
-    );
-  }
+        </Tab>
+      </Tabs>
+    </div>
+  );
 };
 
 export default ResultList;
