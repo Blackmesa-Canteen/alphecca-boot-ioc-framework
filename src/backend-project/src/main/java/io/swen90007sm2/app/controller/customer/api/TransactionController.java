0f720e6fb5f9b5448b55675fe0c93e8508c07215
996Worker <69796042+Blackmesa-Canteen@users.noreply.github.com>
2022-10-14 22:23:05 +1100

    fix bookHotelWithVersionAndLock param

--- src/backend-project/src/main/java/io/swen90007sm2/app/controller/customer/api/TransactionController.java
+++ src/backend-project/src/main/java/io/swen90007sm2/app/controller/customer/api/TransactionController.java
@@ -12,6 +12,7 @@ import io.swen90007sm2.alpheccaboot.common.constant.RequestMethod;
 import io.swen90007sm2.app.blo.IRoomBlo;
 import io.swen90007sm2.app.blo.ITransactionBlo;
 import io.swen90007sm2.app.model.entity.Room;
+import io.swen90007sm2.app.model.param.CrateTransactionParamV2;
 import io.swen90007sm2.app.model.param.CreateTransactionParam;
 import io.swen90007sm2.app.model.param.CustomerUpdateRoomOrderParam;
 import io.swen90007sm2.app.model.vo.TransactionVo;
@@ -115,12 +116,12 @@ public class TransactionController {
 
     @HandlesRequest(path = "/book_version_check", method = RequestMethod.POST)
     @AppliesFilter(filterNames = {SecurityConstant.CUSTOMER_ROLE_NAME})
-    public R bookHotelWithVersionAndLock(HttpServletRequest request, @QueryParam(value = "version") Integer version, @Valid @RequestJsonBody CreateTransactionParam param) {
+    public R bookHotelWithVersionAndLock(HttpServletRequest request, @Valid @RequestJsonBody CrateTransactionParamV2 param) {
         String token = request.getHeader(SecurityConstant.JWT_HEADER_NAME);
         AuthToken authToken = TokenHelper.parseAuthTokenString(token);
         String userId = authToken.getUserId();
         transactionBlo.doMakeBookingWithRoomVersionAndLock(
-                version,
+                param.getVersion(),
                 userId,
                 param.getHotelId(),
                 param.getStartDate(),
